31cb08e3122c937d6780bbcce52cdf540f90ac44
527adwarampudi-design <527adwarampudi@frhsd.com>
2025-11-13 03:21:32 +0000

    changes7

--- apps.js
+++ apps.js
@@ -1,18 +1,31 @@
 document.getElementById("start-button").addEventListener("click", function() {
-      // Ask user for words
+      // Ask user for name 
       let name = prompt("Enter a person's name:");
+      // Ask user for adjective
       let adjective1 = prompt("Enter an adjective:");
+      // Ask user for household appliance
       let appliance = prompt("Enter a household appliance:");
+      // Ask user for verb ending in -ing
       let verb1 = prompt("Enter a verb ending in -ing:");
+      // Ask user for random object
       let noun1 = prompt("Enter a random object:");
+      // Ask user for animal
       let animal = prompt("Enter an animal:");
+      // Ask user for adjective
       let adjective2 = prompt("Enter another adjective:");
+      // Ask user for place
       let place = prompt("Enter a random place:");
+      // Ask user for celebrity
       let celebrity = prompt("Enter the name of a celebrity:");
+      // Ask user for noise
       let sound = prompt("Enter a weird noise:");
+      // Ask user for name pasttense verb
       let verb2 = prompt("Enter a past-tense verb:");
+      // Ask user for number
       let number = prompt("Enter a number:");
+      // Ask user for food
       let food = prompt("Enter a food:");
+      // Ask user for something you would yell
       let exclamation = prompt("Enter something you'd yell:");
 
       // Build the story
